be3e85d6a8244eb24d0a483869238291da7da013
Rohan Kulkarni <rohankullkarni0@gmail.com>
2026-03-08 23:35:07 +0530

    dashboard commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3306;
 
 const server = http.createServer((req, res) => {
 
@@ -43,14 +44,41 @@ const server = http.createServer((req, res) => {
     });
   }
 
+  //Profile
+    else if (req.url === '/profile' && req.method === 'GET') { 
+    const filePath = path.join(__dirname, 'profile-2.html');
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading profile page');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+  });
+}
+
+  // Settings
+  else if (req.url === '/settings' && req.method === 'GET') { 
+    const filePath = path.join(__dirname, 'settings-2.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading settings page');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+  });
+}
   // 404
   else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Page Not Found');
   }
-
 });
 
-server.listen(3306, () => {
+server.listen(PORT, () => {
   console.log('Server running on http://localhost:3306');
 });
